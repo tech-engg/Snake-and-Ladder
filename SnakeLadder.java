5567a6e5b551b1d65bb27a8ec83b66c7f1a5bd93
tech-engg <techsolution.enggn@gmail.com>
2021-05-21 14:38:17 +0530

    addedRandom

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,11 +1,16 @@
 package com.snakeladder;
 
+import java.lang.Math;
 public class SnakeLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Snake and Ladder");
 		
 		int startPositon =0;
+		
+		int  rollDie = (int) (Math.random()*6)+1;
+		
+		System.out.println("rollDie"  +rollDie);
 	}
 
 }
